d958e4a150c14ab8eac1e013a2530085d69c06cc
Atılberk Çelebi <atilberkcelebi@gmail.com>
2018-04-25 23:43:20 +0300

    pre-submission

--- main.jl
+++ main.jl
@@ -1,25 +1,5 @@
 using Distances
 
-# Maximum number of iterations
-MAX_ITER = 10
-# Maximum number of iterations without improvement
-MAX_PATIENCE = 1
-# minimum euclidean distance for convergence
-CONVERGE_THRESHOLD = 1.0
-# change up to three customers
-MAX_NEIGHBORHOOD = 3
-# 1 : Basic (with local search)
-# 2 : Reduced (with shaking only)
-MODE_SELECT = 1
-MODE = [:Basic, :Reduced][MODE_SELECT]
-
-# how many kmeans trial for vns initial solution
-INIT_TRIAL = 10;
-
-rawdata = readdlm("p654.txt")
-n = rawdata[1,1]
-data = Array{Float64}(rawdata[2:n+1,1:2])
-
 function kmeans(data,k,initcentroids)
     centroids = initcentroids # != nothing ? initcentroids : findCentroids(data, initlabels(size(data,1), k), k)
     oldCentroids = nothing
@@ -46,22 +26,23 @@ function vns(data,numFacility,initcentroids)
     oldCentroids = nothing
     iteration = iteration_wo_improvement = 0
     while iteration < MAX_ITER && iteration_wo_improvement < MAX_PATIENCE
-        @show iteration, iteration_wo_improvement = iteration + 1, iteration_wo_improvement + 1
+        iteration, iteration_wo_improvement = iteration + 1, iteration_wo_improvement + 1
         oldCentroids = centroids
-        # println("vn:",evaluate(data,centroids))
+        #println(evaluate(data,centroids))
         k = 1
         while k <= MAX_NEIGHBORHOOD
             newCentroids = getSomeSolution(data,centroids,N[k])
             if improved(data, newCentroids, centroids)
-                # println("v$k:", evaluate(data,newCentroids))
                 centroids = newCentroids
-                k = 1
-                iteration_wo_improvement = 0
+                break
             else
-                # println("no better :(")
                 k = k + 1
             end
         end
+        if k <= MAX_NEIGHBORHOOD
+            iteration_wo_improvement = 0
+        end
+        append!(KVALUES, k)
     end
     centroids
 end
@@ -74,30 +55,18 @@ improved(d,nc,oc) = evaluate(d,nc) < evaluate(d,oc)
 function localSearch(data,labels,numFacility)
     oldLabels = nothing
     iteration = 0
+    centroids = findCentroids(data,labels,numFacility)
     while iteration < MAX_ITER && labels != oldLabels
         iteration = iteration + 1
         oldLabels = labels
         N = allLocalNeigbours(labels,numFacility)
         bestNeighbour = N[indmin((l->evaluate(data,findCentroids(data,l,numFacility))).(N))]
-        if improved(data, findCentroids(data,bestNeighbour,numFacility), findCentroids(data,labels,numFacility))
-            # newCentroids = findCentroids(data,bestNeighbour,numFacility)
-            # println("ls:",evaluate(data,newCentroids))
+        bestCentroids=findCentroids(data,bestNeighbour,numFacility)
+        if improved(data, bestCentroids, centroids)
             labels = bestNeighbour
-        # else
-        #     println("local minima!")
+            centroids = bestCentroids
+            #println(evaluate(data,centroids))
         end
-        #=
-        for neighbour in N
-            if improved(data, findCentroids(data,neighbour,numFacility), findCentroids(data,labels,numFacility))
-                newCentroids = findCentroids(data,neighbour,numFacility)
-                println(" ls:",evaluate(data,newCentroids))
-                labels = neighbour
-                break
-            # else
-            #     println("local minima!")
-            end
-        end
-        =#
     end
     labels
 end
@@ -106,3 +75,44 @@ end
 #allChangeSet(n,k)= k>1 ? expandN(1:n)(allChangeSet(n,k-1)) : [[i] for i in 1:n]
 allLocalNeigbours(l,n)= filter(ll->ll!=l,[[l[1:i-1];j;l[i+1:end]] for j in 1:n, i in indices(l,1)][:])
 Base.isless(a::Array,b::Array)=true
+
+# Maximum number of iterations
+MAX_ITER = 10
+# Maximum number of iterations without improvement
+MAX_PATIENCE = 3
+# minimum euclidean distance for convergence
+CONVERGE_THRESHOLD = 0.0
+# change up to three customers
+MAX_NEIGHBORHOOD = 3
+# 1 : Basic (with local search)
+# 2 : Reduced (with shaking only)
+MODE_SELECT = 1
+MODE = [:Basic, :Reduced][MODE_SELECT]
+
+# how many kmeans trial for vns initial solution
+INIT_TRIAL = 10;
+
+rawdata = readdlm("p654.txt")
+n = rawdata[1,1]
+data = Array{Float64}(rawdata[2:n+1,1:2])
+
+KARRAY=[]
+KVALUES=[]
+
+function main(numFacilities=[3,5,8], numTrials=5)
+    global KARRAY=[]
+    for nf in numFacilities
+        for i in 1:numTrials
+            global KVALUES=[]
+            il=initlabels(nf,size(data,1))
+            ic=findCentroids(data,il,nf)
+            t0=time()
+            reskms=evaluate(data,kmeans(data,nf,ic))
+            t1=time()
+            resvns=evaluate(data,vns(data,nf,ic))
+            t2=time()
+            println("$nf\t$i\t$reskms\t$(@sprintf("%.3f",1000*(t1-t0))) ms\t$resvns\t$(@sprintf("%.3f",t2-t1)) s")
+            append!(KARRAY,[KVALUES])
+        end
+    end
+end
